ab7eb73011c9c65afbfbca492c6a332bdf6f6a61
FranciscoFlores1998 <francisco.flores1601@alumnos.ubiobio.cl>
2024-09-10 10:56:08 -0300

    Login

---
+++ src/Components/Usuarios/Login.jsx
@@ -0,0 +1,29 @@
+import React, { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+
+export function Login() {
+    const navigate = useNavigate();
+    return (
+        <Fragment>
+
+            <div class="p-3 mb-2 bg-dark text-white" style={{padding:500000}}>
+                <img src="logo.jpeg" className="rounded mx-auto d-block" width={300} height={150} alt="..." />
+                <h1 className="text-center" style={{padding:20}}>Please sign in</h1>
+                <div className="container text-center  " >
+                    <div className="row justify-content-center"style={{padding:10}}>
+                        <div className="col-auto">
+                            <input type="text" className="form-control" placeholder="Email Adress" aria-label="Username" aria-describedby="basic-addon1"></input>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center"style={{padding:10}}>
+                        <div className=" col-auto">
+                            <input type="password" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1"></input>
+                        </div>
+                    </div>
+                    <button className="btn btn-primary" onClick={() => navigate("/home")}>Sign in</button>
+                </div>
+            </div>
+        </Fragment>
+
+    );
+}
