b6233740ee1b7387141d01d2edee5901f1382572
AVnico <nicolas.araya1901@alumnos.ubiobio.cl>
2024-09-10 10:54:20 -0300

    vista-header

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,21 @@ import React from "react";
 import { Route,Routes,useNavigate } from "react-router-dom";
 
 import { Fragment } from "react";
-import { Navbar } from "./Navbar";
-import { ListFacebook } from "./Components/ListFacebook";
-import { ListCard } from "./Components/ListCard";
-import { ListTwitter } from "./Components/ListTwitter";
-import { ListInstagram } from "./Components/ListInstagram";
-import { ListYoutube } from "./Components/ListYoutube";
+import { HeaderG } from "./Components/Busqueda/HeaderG";
+import { Lista } from "./Components/visualPeliculas/Lista";
+import {Login } from "./Components/Usuarios/Login";
 export function App(){
 
     const navigate = useNavigate();
 
     return(
     <Fragment>
-        <Navbar></Navbar>
+        
+
         <Routes>
-            
-            <Route path="/facebook" element={<ListFacebook />} />
-            <Route path="/twitter" element={<ListTwitter />} />
-            <Route path="/instagram" element={<ListInstagram />} />
-            <Route path="/youtube" element={<ListYoutube />} />
-            <Route path="*" element={<ListCard/>} />
+
+        <Route path="*" element={<Login />} />
+
         </Routes>
     </Fragment>);
 }
